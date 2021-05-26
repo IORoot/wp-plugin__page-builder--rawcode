1344daf3f2d8f23f908dc78f1959c735248c6ee2
Andy Pearson <andy@londonparkour.com>
2021-05-26 10:51:11 +0100

    remove shortcode

--- src/moustaches/random_image_url.php
+++ src/moustaches/random_image_url.php
@@ -5,10 +5,6 @@ namespace andyp\pagebuilder\rawcode\moustaches;
 /**
  *  Matches {{random_image_url:4,67,1,73,2}}
  * 
- * or via shortcode with:
- * 
- * [random_image_url ids="4,67,1,73,2"]
- * 
  * CSV of image IDs.
  */
 class random_image_url
@@ -17,16 +13,7 @@ class random_image_url
     public function __construct()
     {
         add_filter('rawcode_moustaches', [$this, 'filter_code'], 10, 1);
-        add_shortcode('random_image_url', [$this, 'shortcode_code']);
     }
-    
-    public function shortcode_code($atts, $content, $shortcode_tag)
-    {
-        $code = '{{random_image_url:'.$atts['ids'].'}}';
-        $url = $this->filter_code($code);
-        return $url;
-    }
-
 
     public function filter_code($code)
     {
